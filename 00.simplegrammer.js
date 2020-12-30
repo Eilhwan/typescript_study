f36009ca7fd3eb6eb1775eeb5f536551af7c056d
Eilhwan <eilhwan@gmail.com>
2020-12-30 13:37:24 +0900

    12.30 typescript getting started

---
+++ 00.simplegrammer.js
@@ -0,0 +1,21 @@
+
+
+// 비구조화 할당 <- js
+let person = {
+    name: 'jane',
+    age: 22
+};
+
+let {name, age} = person;
+
+let array = [1, 2, 3, 4]
+let [head, ...rest] = array
+
+let a = 1, b = 2
+
+[b, a] = [a, b]
+
+// Arrow function
+const add = (a, b) => a + b;
+
+
